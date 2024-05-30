5d83ed1e21def6c4e8d5dc8df900a4448c36695b
artFetisov <fetishfestoff@mail.ru>
2024-05-30 14:49:13 +0300

    add config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function start() {
+  const PORT = process.env.PORT || 4999;
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  console.log(process.env.NODE_ENV);
+
+  // app.use(corsMiddleware);
+
+  // app.useGlobalPipes(new ValidationPipe());
+
+  // app.enableCors({
+  //   origin: [
+  //     'https://client-todo-microsoft.vercel.app',
+  //     'http://localhost:4200',
+  //   ],
+  //   methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+  //   credentials: true,
+  //   optionsSuccessStatus: 204,
+  //   allowedHeaders:
+  //     'Origin, Authorization, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+  // });
+
+  app.setGlobalPrefix('api');
+
+  app.listen(PORT, () => {
+    console.log(`Server started on PORT ${PORT}`);
+  });
 }
-bootstrap();
+
+start();
